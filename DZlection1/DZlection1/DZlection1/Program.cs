58684e3607b7a67c6d92ada4e0f3ed3e29bd354a
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-19 20:21:53 +0400

    DZ lection1

---
+++ DZlection1/DZlection1/DZlection1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZlection1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            double value = a * b / (c + d);
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZlection1/Program.cs
@@ -0,0 +1,7 @@
+﻿// print a*b/c+d 
+int a = int.Parse(Console.ReadLine);
+int b = int.Parse(Console.ReadLine);
+int c = int.Parse(Console.ReadLine);
+int d = int.Parse(Console.ReadLine);
+double formule = a*b/(c+d);
+Console.WriteLine(formule);
