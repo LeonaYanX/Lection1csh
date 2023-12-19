f3d5bee8dfda6a60c3c9f0099113ca8189d6638e
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-19 19:53:59 +0400

    task2done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,16 @@
  int b = 56;
  int sum = a + b;
  Console.WriteLine(sum);
+ long l = 11;
+ long sum2 = l + a;
+ Console.WriteLine(sum2);
+ double d = 5.5;
+ double sum3 = d + a;
+ Console.WriteLine(sum3);
+ int product1 = a*b;
+ long product2 = l*a;
+ double product3 = d*a;
+ Console.WriteLine(product1);
+ Console.WriteLine(product2);
+ Console.WriteLine(product3);
+
