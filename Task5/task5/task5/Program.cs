719cd7411ec9a5bb55cf5a9f8e092c70edcaa307
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-19 20:44:56 +0400

    Task5done

---
+++ Task5/task5/task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me how long I must count?");
+            int N = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= N) 
+            {
+                Console.Write(i);
+                Console.Write(' ');
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
